685fd81af22875f57a02996fb3ffc6b449cafde9
arthur.jovita <arthur.jovita@adevinta.com>
2022-08-14 20:25:46 +0200

    fix: fix typo

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -9,7 +9,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64
+    ami_type = "AL2_x86_64"
 
     attach_cluster_primary_security_group = true
 
